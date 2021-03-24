50065ef699b6f916fbf577f9da335b1b86a28755
Yann Solol <37675007+maxx6262@users.noreply.github.com>
2021-03-24 13:03:43 +0100

    Update getAccount.js

--- getAccount.js
+++ getAccount.js
@@ -1,31 +1,27 @@
-const Web3 = require('web3')
+onst Web3 = require('web3')
 const fs = require('fs')
 const path = require('path')
 var web3 = new Web3()
-
-
+const privateKey = '0xbf163c12734401778c593794eeb19353fda8da1abf4cdc731cc5b64fdcbc5dbc'
 const filePath = path.join(__dirname, './.secret')
-
 function getAccount() {
     return new Promise(resolve => {
-        if(fs.existsSync(filePath)){
-            fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
-                resolve(web3.eth.accounts.privateKeyToAccount(data))
-            })
-        } else {
-            let randomAccount = web3.eth.accounts.create()
-        
-            fs.writeFile(filePath, randomAccount.privateKey, (err) => {
-                if(err) {
-                    return console.log(err);
-                }
-            })
-
-            resolve(randomAccount)
-        }
+//        if(fs.existsSync(filePath)){
+//            fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+//                resolve(web3.eth.accounts.privateKeyToAccount(data))
+//            })
+//        } else {
+//            let randomAccount = web3.eth.accounts.create()
+//            fs.writeFile(filePath, randomAccount.privateKey, (err) => {
+          let randomAccount = web3.eth.accounts.privateKeyToAccount(privateKey);
+//if(err) {
+//                    return console.log(err);
+//                }
+//            })
+          resolve(randomAccount)
+//        }
     })
 }
-
 module.exports = {
     getAccount
 }
